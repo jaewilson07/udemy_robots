aa068a3d2d7020749584228d2f816f10c3db429f
Jae Wilson <jaewilson07@gmail.com>
2020-07-05 17:09:21 +0200

    Updates

---
+++ precache-manifest.91cabd2514215330032f31a8f134d1fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5785dc73ed004a535423edee423854f",
+    "url": "/ud_RobotApp/index.html"
+  },
+  {
+    "revision": "c1c142dd2ddaa6d8b568",
+    "url": "/ud_RobotApp/static/css/main.9310e0e5.chunk.css"
+  },
+  {
+    "revision": "c020de8fd3608f0820d5",
+    "url": "/ud_RobotApp/static/js/2.8d8ea097.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ud_RobotApp/static/js/2.8d8ea097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c142dd2ddaa6d8b568",
+    "url": "/ud_RobotApp/static/js/main.7421e590.chunk.js"
+  },
+  {
+    "revision": "8480862e194621c72c24",
+    "url": "/ud_RobotApp/static/js/runtime-main.7895ec56.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ud_RobotApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
